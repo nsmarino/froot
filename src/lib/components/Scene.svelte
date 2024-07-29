48df4fcb590cfc48ce3274ea0669a6c7d94fa6fa
Nicholas Marino <nsmarino0@gmail.com>
2024-07-29 18:46:21 -0400

    Can!

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,7 +1,17 @@
 <script>
   import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+  import { ContactShadows, Float, Grid, OrbitControls, Environment } from '@threlte/extras'
+
+  import Model from "./models/Gleem_Can_DragonFruitMango.svelte"
 </script>
+
+<Environment
+  path="/envmap/"
+  files="overcast.hdr"
+  isBackground={false}
+  format="hdr"
+  groundProjection={{ radius: 200, height: 5, scale: { x: 100, y: 100, z: 100 } }}
+/>
 
 <T.PerspectiveCamera
   makeDefault
@@ -10,19 +20,18 @@
 >
   <OrbitControls
     autoRotate
-    enableZoom={false}
+    enableZoom={true}
     enableDamping
     autoRotateSpeed={0.5}
-    target.y={1.5}
+    target.y={1.75}
   />
 </T.PerspectiveCamera>
 
 <T.DirectionalLight
-  intensity={0.8}
+  intensity={0.5}
   position.x={5}
   position.y={10}
 />
-<T.AmbientLight intensity={0.2} />
 
 <Grid
   position.y={-0.001}
@@ -44,38 +53,7 @@
   floatIntensity={1}
   floatingRange={[0, 1]}
 >
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
+  <Model scale={0.5} />
 </Float>
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
